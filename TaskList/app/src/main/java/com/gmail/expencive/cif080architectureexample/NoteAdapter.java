06be8d4e731b7d2cc50be031efa02dc25a572b19
expencive <43270198+expencive@users.noreply.github.com>
2019-03-13 13:00:28 +0300

    TaskList

---
+++ TaskList/app/src/main/java/com/gmail/expencive/cif080architectureexample/NoteAdapter.java
@@ -0,0 +1,119 @@
+package com.gmail.expencive.cif080architectureexample;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NoteAdapter extends ListAdapter<Note, NoteAdapter.NoteHolder> {
+
+
+    private OnItemClickListener listener;
+
+    public NoteAdapter() {
+        super(DIFF_CALLBACK);
+    }
+
+    private static final DiffUtil.ItemCallback<Note> DIFF_CALLBACK = new DiffUtil.ItemCallback<Note>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull Note oldItem, @NonNull Note newItem) {
+            return oldItem.getId() == newItem.getId();
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Note oldItem, @NonNull Note newItem) {
+            return oldItem.getTitle().equals(newItem.getTitle()) && oldItem.getDescription().equals(newItem.getDescription())
+                    && oldItem.getPriority() == newItem.getPriority();
+        }
+    };
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_item, parent, false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        Note currentNote = getItem(position);
+        holder.textViewTitle.setText(currentNote.getTitle());
+        holder.textViewDescription.setText(currentNote.getDescription());
+        holder.textViewPriority.setText(String.valueOf(currentNote.getPriority()));
+
+        switch (currentNote.getPriority()){
+            case 1:
+                holder.itemView.setBackgroundColor(Color.parseColor("#E0F7FA"));
+                break;
+            case 2:
+                holder.itemView.setBackgroundColor(Color.parseColor("#E8F5E9"));
+                break;
+            case 3:
+                holder.itemView.setBackgroundColor(Color.parseColor("#F9FBE7"));
+                break;
+            case 4:
+                holder.itemView.setBackgroundColor(Color.parseColor("#FBE9E7"));
+                break;
+            case 5:
+                holder.itemView.setBackgroundColor(Color.parseColor("#FDDEE8"));
+                break;
+
+        }
+
+
+
+    }
+
+
+
+
+
+    public Note getNoteAt(int position) {
+        return getItem(position);
+
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        private TextView textViewTitle, textViewDescription, textViewPriority;
+
+        public NoteHolder(@NonNull final View itemView) {
+            super(itemView);
+
+
+
+            textViewTitle = itemView.findViewById(R.id.text_view_title);
+            textViewDescription = itemView.findViewById(R.id.text_view_id_description);
+            textViewPriority = itemView.findViewById(R.id.text_view_priority);
+
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (listener != null && position != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(getItem(position));
+                    }
+                }
+            });
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Note note);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+
+    }
+}
